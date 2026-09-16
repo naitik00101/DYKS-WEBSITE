3f3699957a06e82ba9f3438371b4a513b6d91c05
naitik00101 <naitikmorkar@gmail.com>
2026-09-16 23:33:34 +0530

    fix: Next.js config

--- package/next.config.js
+++ package/next.config.js
@@ -1,5 +1,3 @@
-const { withNextLocator } = require('@next-locator/babel-plugin/dist/config.js')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -7,7 +5,16 @@ const nextConfig = {
   },
 }
 
-const config = withNextLocator(nextConfig)
-delete config.turbopack
+let config = nextConfig
+
+if (process.env.NODE_ENV === 'development') {
+  try {
+    const { withNextLocator } = require('@next-locator/babel-plugin/dist/config.js')
+    config = withNextLocator(nextConfig)
+    if (config.turbopack) delete config.turbopack
+  } catch (e) {
+    config = nextConfig
+  }
+}
 
 module.exports = config
